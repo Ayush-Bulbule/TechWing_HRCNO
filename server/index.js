c1a9acd3e6fd15aad1d435a0ba4b062580f1989c
Mustafa <100032108+TechyMT@users.noreply.github.com>
2023-12-26 21:39:43 +0530

    Refactor server setup and add ping endpoint

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const WebSocket = require('ws');
 const http = require('http');
-const express = require("express");
-const app = express();
-const server = http.createServer(app);
+const server = http.createServer();
 const wss = new WebSocket.Server({ server, clientTracking: true });
 
 app.use(express.json());
